757ab849041fdb6d42dc0dba49e67a4e1df3e36d
smithhan02 <96280428+smithhan02@users.noreply.github.com>
2023-03-22 18:49:17 -0600

    first commit

---
+++ backend/newMission12_hls68/newMission12_hls68/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace newMission12_hls68
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
